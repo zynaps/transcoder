e84881b25d8b321eb31f386cf22dba2ad47ad0c4
Igor Vinokurov <zynaps@zynaps.ru>
2018-12-11 04:19:21 +0300

    using linter recommendations

--- transcode.sh
+++ transcode.sh
@@ -4,50 +4,50 @@ set -x
 
 logger()
 {
-  echo $(date +'%b %d %T') $1 | tee -a /log/transcode.log
+  echo "$(date +'%b %d %T')" ${1} | tee -a /log/transcode.log
 }
 
 inotifywait -m /watch -e moved_to --timefmt '%FT%TZ' --format '%T %f' | \
   while read timestamp name; do
-    uid=`pwgen -A1 6`
+    uid=$(pwgen -A1 6)
 
-    logger "$uid: detected \"$name\" at $timestamp"
+    logger "${uid}: detected \"${name}\" at ${timestamp}"
 
     if [[ -d "/watch/${name}" ]]; then
       mv "/watch/${name}/"*.avi /watch
-      logger "$uid: moved original names from subdir"
+      logger "${uid}: moved original names from subdir"
       rm -rf "/watch/${name}"
     elif [[ -f "/watch/${name}" ]]; then
       if [[ ${name##*.} = 'avi' ]]; then
         mv "/watch/${name}" /output
-        logger "$uid: original file moved to target location"
+        logger "${uid}: original file moved to target location"
 
-        logger "$uid: transcoding original file"
+        logger "${uid}: transcoding original file"
 
         output=${name%.avi}.m4v
 
-        if [[ -f "/temp/$output" ]]; then
-          rm -f "/temp/$output"
-          logger "$uid: delete stalled file from previous transcoding"
+        if [[ -f "/temp/${output}" ]]; then
+          rm -f "/temp/${output}"
+          logger "${uid}: delete stalled file from previous transcoding"
         fi
 
         HandBrakeCLI \
-          --input "/output/$name" --output "/temp/$output" \
+          --input "/output/${name}" --output "/temp/${output}" \
           --preset "Apple 1080p30 Surround" --crop 0:0:0:0 --optimize
 
         if [[ $? -eq 0 ]]; then
-          mv -f "/temp/$output" /output
+          mv -f "/temp/${output}" /output
 
-          name_size=`stat -c%s "/output/$name"`
-          output_size=`stat -c%s "/output/$output"`
+          name_size=$(stat -c%s "/output/${name}")
+          output_size=$(stat -c%s "/output/${output}")
 
-          logger "$uid: transcoded, saved `expr $name_size - $output_size` bytes"
+          logger "${uid}: transcoded, saved $((name_size-output_size)) bytes"
 
-          rm -f "/output/$name"
-          logger "$uid: original file removed"
+          rm -f "/output/${name}"
+          logger "${uid}: original file removed"
         else
-          rm -f "/temp/$output"
-          logger "$uid: error transcoding, tempfile deleted"
+          rm -f "/temp/${output}"
+          logger "${uid}: error transcoding, tempfile deleted"
         fi
       fi
     fi
