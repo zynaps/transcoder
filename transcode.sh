aab6b7dcc5077e986cab5e9108979a547f2e5392
Igor Vinokurov <zynaps@zynaps.ru>
2018-12-18 10:38:25 +0300

    curated support for subtitles

--- transcode.sh
+++ transcode.sh
@@ -12,15 +12,23 @@ while true; do
 
         [[ -f "/temp/${output}" ]] && rm -f "/temp/${output}"
 
+        unset SUBTITLES_OPTS
+
+        subtitles=${name%.avi}.srt
+
+        if [[ -f "/output/${subtitles}" ]]; then
+            SUBTITLES_OPTS="--srt-file /output/${subtitles} --srt-codeset=UTF-8 --srt-default"
+        fi
+
         HandBrakeCLI \
             --input "/output/${name}" --output "/temp/${output}" \
             --preset "Apple 1080p30 Surround" --crop 0:0:0:0 --optimize \
-            --verbose=0
+            --verbose=0 ${SUBTITLES_OPTS}
 
         if [[ $? -eq 0 ]]; then
-            mv -f "/temp/${output}" /output && rm -f "/output/${name}"
+            mv -f "/temp/${output}" /output && rm -f "/output/${name}" "/output/${subtitles}"
         else
-            rm -f "/temp/${output}" && mv "/output/${name}" /deferred
+            rm -f "/temp/${output}" && mv -f "/output/${name}" "/output/${subtitles}" /deferred
         fi
     else
         inotifywait -e moved_to /watch
